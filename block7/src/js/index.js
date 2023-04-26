fab8ecb37de7d6d99c66c5453c7562d3ced2e91b
vaffa201 <qwery201@mail.ru>
2023-04-27 00:16:56 +0300

    fix: some of the comments have been corrected

--- block7/src/js/index.js
+++ block7/src/js/index.js
@@ -2,9 +2,49 @@ import '../scss/style.scss'
 
 const descriptionText = document.querySelector('.description__text');
 const descriptionShowAll = document.querySelector('.description__btn');
-descriptionShowAll.onclick = function () {
-    descriptionText.classList.toggle('description__text--extended')
+
+const brandsList = document.querySelector('.brands__list');
+const brandItemTemplate = document.querySelector('.brand-template').content;
+const newBrandTemplate = brandItemTemplate.querySelector('.brands__item');
+const brandShowAll = document.querySelector('.brands__btn');
+const brandShowAllText = brandShowAll.querySelector('.show-all-btn__text');
+const brandShowAllIcon = brandShowAll.querySelector('.show-all-btn__icon');
+
+const menuButton = document.querySelector('.header__burger-button');
+const menuButtonIcon = menuButton.querySelector('.burger-icon');
+
+const main = document.querySelector('.main');
+const header = document.querySelector('.header')
+const asideMenu = document.querySelector('.aside-menu');
+const headerActions = document.querySelector('.header__actions');
+
+const feedbackButtons = document.querySelectorAll('.contact-button2');
+const asideFeedback = document.querySelector('.aside-feedback');
+const closeFeedback = asideFeedback.querySelector('.aside-feedback__close-button');
+const firstFeedbackInput = asideFeedback.querySelector('.aside-feedback__input');
+
+const asideRecall = document.querySelector('.aside-recall');
+const recallButtons = document.querySelectorAll('.contact-button1');
+const closeRecall = asideRecall.querySelector('.aside-recall__close-button');
+const firstRecallInput = asideRecall.querySelector('.aside-recall__input');
+
+
+const tecTypeList = document.querySelector('.tec-types__list');
+const tecTypeItemTemplate = document.querySelector('.tec-type-template').content;
+const newTypeTemplate = tecTypeItemTemplate.querySelector('.tec-types__item');
+const tecTypeShowAll = document.querySelector('.tec-types__btn');
+const tecTypeShowAllText = tecTypeShowAll.querySelector('.show-all-btn__text');
+const tecTypeShowAllIcon = tecTypeShowAll.querySelector('.show-all-btn__icon');
+
+const priceList = document.querySelector('.price__list');
+const priceItemTemplate = document.querySelector('.price-item-template').content;
+const newPriceTemplate = priceItemTemplate.querySelector('.price__item');
+
+const showDescriptionText = function () {
+    descriptionText.classList.toggle('description__text--extended');
 }
+
+descriptionShowAll.addEventListener('click', showDescriptionText)
 
 const brands = [
     './img/brands/Apple.png',
@@ -20,16 +60,7 @@ const brands = [
     './img/brands/ViewSonic.png'
 ];
 
-const brandsList = document.querySelector('.brands__list');
-
-const brandItemTemplate = document.querySelector('.brand-template').content;
-const newBrandTemplate = brandItemTemplate.querySelector('.brands__item');
-
-const brandShowAll = document.querySelector('.brands__btn');
-const brandShowAllText = brandShowAll.querySelector('.show-all-btn__text');
-const brandShowAllIcon = brandShowAll.querySelector('.show-all-btn__icon');
-
-brandShowAll.onclick = function () {
+const showBrands = function () {
     brandsList.classList.toggle('brands__list--extended');
     if (brandsList.classList.contains('brands__list--extended')) {
         brandShowAllText.textContent = 'Скрыть';
@@ -40,6 +71,8 @@ brandShowAll.onclick = function () {
     }
 }
 
+brandShowAll.addEventListener('click', showBrands);
+
 for (let link of brands) {
     let brand = newBrandTemplate.cloneNode(true);
     const brandImage = brand.querySelector('.brands__image');
@@ -48,19 +81,7 @@ for (let link of brands) {
     brandsList.appendChild(brand);
 }
 
-
-// =======================================================================
-
-const menuButton = document.querySelector('.header__burger-button');
-const menuButtonIcon = menuButton.querySelector('.burger-icon');
-
-const main = document.querySelector('.main');
-const header = document.querySelector('.header')
-const asideMenu = document.querySelector('.aside-menu');
-const headerActions = document.querySelector('.header__actions');
-
-
-menuButton.onclick = function () {
+const toggleMenu = function () {
     menuButtonIcon.classList.toggle('burger-icon--active');
     document.body.classList.toggle('body--lock');
     asideMenu.classList.toggle('aside-menu--active');
@@ -68,61 +89,61 @@ menuButton.onclick = function () {
     headerActions.classList.toggle('header__actions--blured');
 }
 
+menuButton.addEventListener('click', toggleMenu);
+
+const openFeedback = function () {
+    if (main.classList.contains('main--blured')) {
+        asideMenu.classList.remove('aside-menu--active');
+        menuButtonIcon.classList.remove('burger-icon--active');
+        asideFeedback.classList.add('aside-feedback--active');
+        headerActions.classList.remove('header__actions--blured');
+        header.classList.add('header--blured');
+    } else {
+        main.classList.add('main--blured');
+        document.body.classList.add('body--lock');
+        header.classList.add('header--blured');
+
+        if (mediaQueryDesktop.matches) {
+            asideMenu.classList.add('aside-menu--blured');
+        }
+        asideFeedback.classList.add('aside-feedback--active');
+        firstFeedbackInput.focus();
+    }
+}
+
+const openRecall = function () {
+    if (main.classList.contains('main--blured')) {
+        asideMenu.classList.remove('aside-menu--active');
+        menuButtonIcon.classList.remove('burger-icon--active');
+        asideFeedback.classList.add('aside-feedback--active');
+        headerActions.classList.remove('header__actions--blured');
+        header.classList.add('header--blured');
+    } else {
+        main.classList.add('main--blured');
+        document.body.classList.add('body--lock');
+        header.classList.add('header--blured');
+
+        if (mediaQueryDesktop.matches) {
+            asideMenu.classList.add('aside-menu--blured');
+        }
+        asideRecall.classList.add('aside-recall--active');
+        firstRecallInput.focus();
+    }
+}
+
 document.addEventListener('click', (evt) => {
-    const clickMenu = evt.target.contains(asideMenu);
-    if (clickMenu) {
+    const isMenuTarget = evt.target.contains(asideMenu);
+    if (isMenuTarget) {
         menuButtonIcon.classList.remove('burger-icon--active');
         document.body.classList.remove('body--lock');
         asideMenu.classList.remove('aside-menu--active');
         main.classList.remove('main--blured');
         headerActions.classList.remove('header__actions--blured');
     }
-})
 
-
-// --------------------------------------------------------------------------
-
-const feedbackButtons = document.querySelectorAll('.contact-button2');
-const asideFeedback = document.querySelector('.aside-feedback');
-const closeFeedback = asideFeedback.querySelector('.aside-feedback__close-button');
-
-
-for (let button of feedbackButtons) {
-    button.onclick = function () {
-        if (main.classList.contains('main--blured')) {
-            asideMenu.classList.remove('aside-menu--active');
-            menuButtonIcon.classList.remove('burger-icon--active');
-            asideFeedback.classList.add('aside-feedback--active');
-            headerActions.classList.remove('header__actions--blured');
-            header.classList.add('header--blured');
-        } else {
-            main.classList.add('main--blured');
-            document.body.classList.add('body--lock');
-            header.classList.add('header--blured');
-
-            if (window.matchMedia("(min-width: 1366px)").matches) {
-                asideMenu.classList.add('aside-menu--blured');
-            }
-
-            asideFeedback.classList.add('aside-feedback--active');
-        }
-    }
-}
-
-closeFeedback.onclick = function () {
-    if (window.matchMedia("(min-width: 1366px)").matches) {
-        asideMenu.classList.remove('aside-menu--blured');
-    }
-    header.classList.remove('header--blured');
-    document.body.classList.remove('body--lock');
-    main.classList.remove('main--blured');
-    asideFeedback.classList.remove('aside-feedback--active');
-}
-
-document.addEventListener('click', (evt) => {
     const clickFeedback = evt.target.contains(asideFeedback);
     if (clickFeedback) {
-        if (window.matchMedia("(min-width: 1366px)").matches) {
+        if (mediaQueryDesktop.matches) {
             asideMenu.classList.remove('aside-menu--blured');
         }
         header.classList.remove('header--blured');
@@ -130,52 +151,47 @@ document.addEventListener('click', (evt) => {
         main.classList.remove('main--blured');
         asideFeedback.classList.remove('aside-feedback--active');
     }
+
+    const clickFeedbackButton = evt.target.classList.contains('contact-button2');
+    if (clickFeedbackButton) {
+        openFeedback();
+    }
+
+    const clickRecallButton = evt.target.classList.contains('contact-button1');
+    if (clickRecallButton) {
+        openRecall();
+    }
 })
 
+const mediaQueryDesktop = window.matchMedia("(min-width: 1366px)");
 
-// --------------------------------------------------------------------------
-
-
-const asideRecall = document.querySelector('.aside-recall');
-const recallButtons = document.querySelectorAll('.contact-button1');
-const closeRecall = asideRecall.querySelector('.aside-recall__close-button');
-
-for (let button of recallButtons) {
-    button.onclick = function () {
-        if (main.classList.contains('main--blured')) {
-            asideMenu.classList.remove('aside-menu--active');
-            menuButtonIcon.classList.remove('burger-icon--active');
-            asideFeedback.classList.add('aside-feedback--active');
-            headerActions.classList.remove('header__actions--blured');
-            header.classList.add('header--blured');
-        } else {
-            main.classList.add('main--blured');
-            document.body.classList.add('body--lock');
-            header.classList.add('header--blured');
-
-            if (window.matchMedia("(min-width: 1366px)").matches) {
-                asideMenu.classList.add('aside-menu--blured');
-            }
-
-            asideRecall.classList.add('aside-recall--active');
-        }
+const toggleAside = function () {
+    if (mediaQueryDesktop.matches) {
+        asideMenu.classList.toggle('aside-menu--blured');
     }
+    header.classList.toggle('header--blured');
+    document.body.classList.toggle('body--lock');
+    main.classList.toggle('main--blured');
 }
 
-closeRecall.onclick = function () {
-    if (window.matchMedia("(min-width: 1366px)").matches) {
-        asideMenu.classList.remove('aside-menu--blured');
-    }
-    header.classList.remove('header--blured');
-    document.body.classList.remove('body--lock');
-    main.classList.remove('main--blured');
-    asideRecall.classList.remove('aside-recall--active');
+const closeFeedbackAside = function () {
+    toggleAside();
+    asideFeedback.classList.toggle('aside-feedback--active');
 }
+
+closeFeedback.addEventListener('click', closeFeedbackAside);
+
+const closeRecallAside = function () {
+    toggleAside();
+    asideRecall.classList.toggle('aside-recall--active');
+}
+
+closeRecall.addEventListener('click', closeRecallAside);
 
 document.addEventListener('click', (evt) => {
     const clickRecall = evt.target.contains(asideFeedback);
     if (clickRecall) {
-        if (window.matchMedia("(min-width: 1366px)").matches) {
+        if (mediaQueryDesktop.matches) {
             asideMenu.classList.remove('aside-menu--blured');
         }
         header.classList.remove('header--blured');
@@ -184,8 +200,6 @@ document.addEventListener('click', (evt) => {
         asideRecall.classList.remove('aside-recall--active');
     }
 })
-
-// ===========================================================================
 
 const tecTypes = [
     'Ремонт ноутбуков',
@@ -197,11 +211,6 @@ const tecTypes = [
     'Ремонт мониторов',
 ];
 
-const tecTypeList = document.querySelector('.tec-types__list');
-
-const tecTypeItemTemplate = document.querySelector('.tec-type-template').content;
-const newTypeTemplate = tecTypeItemTemplate.querySelector('.tec-types__item');
-
 for (let text of tecTypes) {
     let tecType = newTypeTemplate.cloneNode(true);
     const tecTypeText = tecType.querySelector('.tec-types__text');
@@ -210,14 +219,7 @@ for (let text of tecTypes) {
     tecTypeList.appendChild(tecType);
 }
 
-// ----------------------------------------------------------------------
-
-const tecTypeShowAll = document.querySelector('.tec-types__btn');
-const tecTypeShowAllText = tecTypeShowAll.querySelector('.show-all-btn__text');
-const tecTypeShowAllIcon = tecTypeShowAll.querySelector('.show-all-btn__icon');
-
-
-tecTypeShowAll.onclick = function () {
+const showTecTypes = function () {
     tecTypeList.classList.toggle('tec-types__list--extended');
     if (tecTypeList.classList.contains('tec-types__list--extended')) {
         tecTypeShowAllText.textContent = 'Скрыть';
@@ -228,9 +230,7 @@ tecTypeShowAll.onclick = function () {
     }
 }
 
-
-// ==========================================================================================
-
+tecTypeShowAll.addEventListener('click', showTecTypes)
 
 const prices = [
     {
@@ -260,11 +260,6 @@ const prices = [
     },
 ];
 
-const priceList = document.querySelector('.price__list');
-
-const priceItemTemplate = document.querySelector('.price-item-template').content;
-const newPriceTemplate = priceItemTemplate.querySelector('.price__item');
-
 for (let price of prices) {
     let priceItem = newPriceTemplate.cloneNode(true);
 
@@ -279,50 +274,44 @@ for (let price of prices) {
     priceList.appendChild(priceItem);
 }
 
-// ===========================================================================================
-
-let isSwiperExist = false;
-let swiperBrands = new Swiper();
-let swiperTecType = new Swiper();
-let swiperPrice = new Swiper();
+const swiperMediaQuery = window.matchMedia("(min-width: 768px)");
+let swiperBrands;
+let swiperTecType;
+let swiperPrice;
 
 let swiperSwitch = function () {
-    if (!window.matchMedia("(min-width: 768px)").matches) {
-        if (!isSwiperExist) {
-            isSwiperExist = true;
+    if (!swiperMediaQuery.matches) {
 
-            swiperBrands = new Swiper('.brands .swiper', {
-                direction: 'horizontal',
-                spaceBetween: 16,
-                slidesPerView: "auto",
+        swiperBrands = new Swiper('.brands .swiper', {
+            direction: 'horizontal',
+            spaceBetween: 16,
+            slidesPerView: "auto",
 
-                pagination: {
-                    el: '.brands .swiper-pagination',
-                },
-            });
+            pagination: {
+                el: '.brands .swiper-pagination',
+            },
+        });
 
-            swiperTecType = new Swiper('.tec-types .swiper', {
-                direction: 'horizontal',
-                spaceBetween: 16,
-                slidesPerView: "auto",
+        swiperTecType = new Swiper('.tec-types .swiper', {
+            direction: 'horizontal',
+            spaceBetween: 16,
+            slidesPerView: "auto",
 
-                pagination: {
-                    el: '.tec-types .swiper-pagination',
-                },
-            });
+            pagination: {
+                el: '.tec-types .swiper-pagination',
+            },
+        });
 
-            swiperPrice = new Swiper('.price .swiper', {
-                direction: 'horizontal',
-                spaceBetween: 16,
-                slidesPerView: "auto",
+        swiperPrice = new Swiper('.price .swiper', {
+            direction: 'horizontal',
+            spaceBetween: 16,
+            slidesPerView: "auto",
 
-                pagination: {
-                    el: '.price .swiper-pagination',
-                },
-            });
-        }
-    } else if (isSwiperExist) {
-        isSwiperExist = false;
+            pagination: {
+                el: '.price .swiper-pagination',
+            },
+        });
+    } else if (swiperBrands || swiperTecType || swiperPrice) {
         swiperBrands.destroy();
         swiperTecType.destroy();
         swiperPrice.destroy();
